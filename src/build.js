c15b1e6dac03c62338f4ceed4de84fc57c562b3d
allpaqa-jgk <49287928+allpaqa-jgk@users.noreply.github.com>
2019-07-13 16:55:06 +0900

    fix build dir temporally disable build of fwindows exe file

--- src/build.js
+++ src/build.js
@@ -4,24 +4,24 @@ const path = require('path')
 
 async function runBuild() {
   await compile({
-    input: path.join(__dirname, '../src/main.js'),
+    input: path.join(__dirname, './main.js'),
     // build: true, // required to use patches
-    targets: 'mac-x64-12.6.0',
+    // targets: 'mac-x64-12.6.0',
     name: 'twitch_text_to_speech_bot',
-    // loglevel: 'verbose',
+    loglevel: 'info',
   }).then(() => {
     console.log('success mac')
   })
 
-  await compile({
-    input: path.join(__dirname, '../src/main.js'),
-    // build: true, // required to use patches
-    target: 'windows-x64-12.6.0',
-    name: 'twitch_text_to_speech_bot',
-    // loglevel: 'verbose',
-  }).then(() => {
-    console.log('success win')
-  })
+  // await compile({
+  //   input: path.join(__dirname, './main.js'),
+  //   // build: true, // required to use patches
+  //   target: 'windows-x64-12.6.0',
+  //   name: 'twitch_text_to_speech_bot',
+  //   // loglevel: 'info',
+  // }).then(() => {
+  //   console.log('success win')
+  // })
 }
 
 runBuild()

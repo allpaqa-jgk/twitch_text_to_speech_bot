4ff53722c7c8ebb36fb91b057d9d790f0b0feca2
allpaqa-jgk <49287928+allpaqa-jgk@users.noreply.github.com>
2019-07-13 16:49:03 +0900

    improve build proccess

--- src/build.js
+++ src/build.js
@@ -2,24 +2,26 @@
 const { compile } = require('nexe')
 const path = require('path')
 
-compile({
-  input: path.join(__dirname, '../src/main.js'),
-  // build: true, // required to use patches
-  target: ['mac-x64-12.6.0', 'windows-x64-12.6.0'], // for mac and windows
-  name: 'twitch_text_to_speech_bot',
-  loglevel: 'verbose',
-  // output: 'twitch_text_to_speech_bot'
-  // target: ['mac-x64-12.6.0'], // for mac
-  // target: ['windows-x64-12.6.0'], // for windows
-  // patches: [
-  //   async (compiler, next) => {
-  //     await compiler.setFileContentsAsync(
-  //       'lib/new-native-module.js',
-  //       'module.exports = 42'
-  //     )
-  //     return next()
-  //   }
-  // ]
-}).then(() => {
-  console.log('success')
-})
+async function runBuild() {
+  await compile({
+    input: path.join(__dirname, '../src/main.js'),
+    // build: true, // required to use patches
+    targets: 'mac-x64-12.6.0',
+    name: 'twitch_text_to_speech_bot',
+    // loglevel: 'verbose',
+  }).then(() => {
+    console.log('success mac')
+  })
+
+  await compile({
+    input: path.join(__dirname, '../src/main.js'),
+    // build: true, // required to use patches
+    target: 'windows-x64-12.6.0',
+    name: 'twitch_text_to_speech_bot',
+    // loglevel: 'verbose',
+  }).then(() => {
+    console.log('success win')
+  })
+}
+
+runBuild()
